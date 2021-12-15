2fd8ebd4cd24e728d6141ea65fbb3358f32b3d46
jerometonnelier <j.tonnelier@kaiman.fr>
2021-12-15 11:39:56 +0100

    cancelled rides not shown

--- Sources/RideHistory/Models/RideHistoryModel.swift
+++ Sources/RideHistory/Models/RideHistoryModel.swift
@@ -14,13 +14,21 @@ import ATACommonObjects
 
 extension PendingPaymentRideData {    
     func attributedString(textColor: UIColor = RideHistoryTabController.conf.palette.mainTexts) -> NSAttributedString {
+        guard let value = value else {
+            return NSAttributedString {
+                AText("-")
+                    .foregroundColor(textColor)
+                    .font(.applicationFont(ofSize: 18, weight: .semibold))
+            }
+        }
         let hasDigits = value - Double(Int(value)) > 0
+        
         return NSAttributedString {
             AText(String(format: hasDigits ? "%0.2f" : "%d", (hasDigits ? value : Int(value))))
                 .foregroundColor(textColor)
                 .font(.applicationFont(ofSize: 18, weight: .semibold))
             
-            AText(unit)
+            AText(unit ?? type.defaultUnit)
                 .foregroundColor(textColor)
                 .font(.applicationFont(ofSize: 12, weight: .semibold))
                 .baselineOffset(6)
